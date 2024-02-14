241e29bdef507a6a40a567dbbd76c073857b0ec6
govin-10 <govindatimsina@outlook.com>
2024-02-14 23:54:06 +0545

    MongoDB connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const PORT_NUMBER = process.env.PORT;
-
+const mongoose = require("mongoose");
+const db_url = process.env.CONNECTION_STRING;
 const express = require("express");
 const app = express();
 
@@ -8,6 +9,15 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT_NUMBER, () => {
+app.listen(PORT_NUMBER, async () => {
+  await mongoose
+    .connect(db_url)
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
+
   console.log(`Server is running on port ${PORT_NUMBER}`);
 });
